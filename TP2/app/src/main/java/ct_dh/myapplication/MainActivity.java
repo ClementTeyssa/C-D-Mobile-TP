9cd699c4e1c023fd4ef8e28d2e133cfffb7de4f4
Clement <clement.teyssandier9@etu.univ-lorraine.fr>
2018-02-12 11:54:47 +0100

    ajout getItem non fonctionnel

--- TP2/app/src/main/java/ct_dh/myapplication/MainActivity.java
+++ TP2/app/src/main/java/ct_dh/myapplication/MainActivity.java
@@ -9,7 +9,11 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<TodoItem> liste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +52,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public ArrayList<TodoItem> getItems() {
+        ArrayList<TodoItem> items = new ArrayList<TodoItem>();
+        items.add(new TodoItem(TodoItem.Tags.Important, "Réviser ses cours"));
+        items.add(new TodoItem(TodoItem.Tags.Normal, "Acheter du pain"));
+        items.add(new TodoItem(TodoItem.Tags.Normal, "Marcher 30 mn par jour"));
+        items.add(new TodoItem(TodoItem.Tags.Faible, "Manger 5 fruits et légumes"));
+        items.add(new TodoItem(TodoItem.Tags.Normal, "Prendre des nouvelles des parents"));
+        items.add(new TodoItem(TodoItem.Tags.Faible, "Préparer la prochaine soirée poker"));
+        items.add(new TodoItem(TodoItem.Tags.Normal, "Révoir les premières saisons de Game of thrones"));
+        items.add(new TodoItem(TodoItem.Tags.Faible, "Finir la vaisselle"));
+        items.add(new TodoItem(TodoItem.Tags.Important, "Acheter un nouveau disque dur"));
+        items.add(new TodoItem(TodoItem.Tags.Important, "Revoir les raccourcis clavier"));
+        items.add(new TodoItem(TodoItem.Tags.Normal, "Tester une nouvelle techno"));
+        items.add(new TodoItem(TodoItem.Tags.Faible, "Tester l'application en cours"));
+
+        return items;
     }
 }
