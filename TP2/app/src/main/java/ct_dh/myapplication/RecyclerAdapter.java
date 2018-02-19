3bde6ede2c7c0a1a1889d9f6b46ec3938b8c068c
David H <david.holzhammer6@etu.univ-lorraine.fr>
2018-02-19 10:32:42 +0100

    Def classe holder

--- TP2/app/src/main/java/ct_dh/myapplication/RecyclerAdapter.java
+++ TP2/app/src/main/java/ct_dh/myapplication/RecyclerAdapter.java
@@ -22,22 +22,41 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.TodoIt
     @Override
     public TodoItemHolder onCreateViewHolder (ViewGroup parent , int viewType ) {
         return null ;
-        }
+    }
 
     @Override
     public void onBindViewHolder ( TodoItemHolder holder , int position ) {
 
-        }
+    }
 
     @Override
     public int getItemCount () {
         return 0;
-        }
+    }
 
     public static class TodoItemHolder extends RecyclerView.ViewHolder {
         public TodoItemHolder ( View itemView ) {
             super (itemView) ;
 
         }
+
+        public View getView ( int position , View convertView , ViewGroup parent ) {
+            ViewHolder holder ;
+
+            if (convertView == null) {
+                convertView = inflat.inflate(R.layout.row, null);
+                holder = new ViewHolder();
+                holder.categ = (TextView)convertView.findViewById( R.id.itemcateg);
+                holder.name = (TextView)convertView.findViewById( R.id.itemname ) ;
+                convertView.setTag(holder);
+                }
+            else
+            holder = (ViewHolder)convertView.getTag();
+                        Product p = ( Product ) getItem(position);
+            holder.categ.setText(p.getCategory());
+            holder.name.setText(p.getTitle());
+                        return convertView ;
+            }
+
     }
 }
